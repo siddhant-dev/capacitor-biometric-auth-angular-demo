f21a9013b0545bde3da4c94b9b633ebbf438176d
Siddhant Jaiswal <siddhant.jaiswal@peluk.org>
2024-07-27 10:22:30 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
-import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import {
+  RouteReuseStrategy,
+  provideRouter,
+  withPreloading,
+  PreloadAllModules,
+} from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { Capacitor } from '@capacitor/core';
+
+const mode = () => {
+  const platform = Capacitor.getPlatform();
+  if(platform === 'android') {
+    return 'md';
+  } else {
+    return 'ios';
+  }
+}
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular(),
+    provideIonicAngular({
+      mode: mode()
+    }),
     provideRouter(routes, withPreloading(PreloadAllModules)),
   ],
 });
+
+
